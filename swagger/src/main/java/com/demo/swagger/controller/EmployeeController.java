c99829fba4f93cf904bcc3c8fff3c96eba9a75f2
omeraltan <omeraltan66@gmail.com>
2024-02-22 08:22:06 +0300

    Commit All

--- swagger/src/main/java/com/demo/swagger/controller/EmployeeController.java
+++ swagger/src/main/java/com/demo/swagger/controller/EmployeeController.java
@@ -1,16 +1,22 @@
 package com.demo.swagger.controller;
 
 import com.demo.swagger.entity.Employee;
+import com.demo.swagger.repository.EmployeeRepository;
 import com.demo.swagger.service.EmployeeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
+/**
+ * Swagger documentation link: http://localhost:8080/swagger-ui.html#/
+ * Swagger editor link       : https://editor.swagger.io/
+ * Api docs                  : http://localhost:8080/v2/api-docs
+ */
 
 @RestController
 @RequestMapping("/employee")
@@ -27,5 +33,37 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeList);
     }
 
+    @GetMapping("{id}")
+    @ApiOperation(value = "Employee information method with id")
+    public ResponseEntity<Optional<Employee>> getEmployeeById(@PathVariable(value = "id") Long id){
+        Optional<Employee> employee = service.findById(id);
+        return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping("/username/{userName}")
+    @ApiOperation(value = "Employee information method with name attribute")
+    public ResponseEntity<Optional<Employee>> getEmployeeWithName(@PathVariable(value = "userName") String userName){
+        Optional<Employee> employee = service.findByUserName(userName);
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("{id}")
+    @ApiOperation(value = "Employee update method")
+    public ResponseEntity<Optional<Employee>> updateEmployee(@PathVariable(value = "id") final Long id, @RequestBody Employee employee){
+        return ResponseEntity.ok(Optional.ofNullable(service.update(employee)));
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Employee create method")
+    public ResponseEntity<Optional<Employee>> createEmployee(@RequestBody Employee employee){
+        return ResponseEntity.ok(Optional.ofNullable(service.save(employee)));
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Employee delete method")
+    public ResponseEntity<Optional<Employee>> deleteEmployee(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- swagger/src/main/java/com/demo/swagger/repository/EmployeeRepository.java
+++ swagger/src/main/java/com/demo/swagger/repository/EmployeeRepository.java
@@ -2,8 +2,15 @@ package com.demo.swagger.repository;
 
 import com.demo.swagger.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByUserName(String userName);
+
 }
